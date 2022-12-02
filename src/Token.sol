6a019f81160f1bc853ba6801a9060bc6ebbb7ad2
wuchuank <wuchuank@gmail.com>
2022-12-02 18:15:12 +0800

    add(contract): skeleton

--- src/Token.sol
+++ src/Token.sol
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
-contract Token is ERC20("Token", "TOK", 18) {
+error NonExistentToken();
 
+contract NFSMembership is ERC721, Ownable {
+    constructor() ERC721("NFS Membership", "NFSM") {}
+
+    function mint(address to, uint256 tokenId) external onlyOwner {
+        _mint(to, tokenId);
+    }
+
+    function tokenURI(uint256 id) public view override returns (string memory) {
+        if (ownerOf(id) == address(0)) {
+            revert NonExistentToken();
+        }
+
+        return string(abi.encode(id));
+    }
 }
