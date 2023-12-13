01935802088e2c0454db134ebf6e0548d171b9f6
wuchuan <wuchuan@skiff.com>
2023-12-13 18:12:42 +0800

    feat: mint logic

--- src/NUSFintech.sol
+++ src/NUSFintech.sol
@@ -3,24 +3,54 @@ pragma solidity ^0.8.17;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
 import {IERC5192} from "ERC5192/src/IERC5192.sol";
+import {ECDSA} from "solady/src/utils/ECDSA.sol";
+import {Owned} from "solmate/auth/Owned.sol";
 
 error TokenLocked();
 error TokenDoesNotExist();
+error InvalidSignature();
 
-contract NUSFintech is ERC721, IERC5192 {
+contract NUSFintech is ERC721, IERC5192, Owned {
+    using ECDSA for bytes32;
+
+    // Soulbound NFT is locked by default
     bool private immutable isLocked = true;
 
-    constructor() ERC721("NUSFintech", "NFS") {}
+    // Address of offchain signer of mint signature
+    address private _offchainSigner;
 
+    // Next token id to be minted
+    uint256 private _currentIndex;
 
-    function tokenURI(uint256 _tokenid) public view override returns (string memory) {
+    // Mapping from token id to department id
+    // Refer to NUSFintechRenderer for more on department id
+    mapping(uint256 => uint256) private _departments;
+
+    constructor() ERC721("NUSFintech", "NFS") Owned(msg.sender) {}
+
+    /// @notice Mints a new NFT with the given department id
+    /// @dev Store department id in _departments mapping for use in tokenURI function
+    /// @param department department id of nft to be minted
+    /// @param _signature signature of mint request
+    function mint(uint256 department, bytes calldata _signature) external {
+        if (!_verifySignature(department, _signature))
+            revert InvalidSignature();
+
+        _departments[_currentIndex] = department;
+        _mint(msg.sender, _currentIndex);
+        ++_currentIndex;
+    }
+
+    function tokenURI(
+        uint256 _tokenid
+    ) public view override returns (string memory) {
         return "";
     }
 
     // =========================================================================
-    // ERC5192 (Minimimal Soulbound NFTs) implementation
+    // ERC5192 (Minimimal Soulbound NFTs) Implementation
     // =========================================================================
-    
+
     modifier checkLock() {
         if (isLocked) revert TokenLocked();
         _;
@@ -32,16 +62,16 @@ contract NUSFintech is ERC721, IERC5192 {
     }
 
     function transferFrom(
-        address from, 
-        address to, 
+        address from,
+        address to,
         uint256 id
     ) public override checkLock {
         super.transferFrom(from, to, id);
     }
 
     function safeTransferFrom(
-        address from, 
-        address to, 
+        address from,
+        address to,
         uint256 id
     ) public override checkLock {
         super.safeTransferFrom(from, to, id);
@@ -60,12 +90,49 @@ contract NUSFintech is ERC721, IERC5192 {
         super.approve(spender, id);
     }
 
-    function setApprovalForAll(address operator, bool approved) public override checkLock {
+    function setApprovalForAll(
+        address operator,
+        bool approved
+    ) public override checkLock {
         super.setApprovalForAll(operator, approved);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IERC5192).interfaceId
-        || super.supportsInterface(interfaceId);
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override returns (bool) {
+        return
+            interfaceId == type(IERC5192).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    // =========================================================================
+    // Ownable Functions
+    // =========================================================================
+    function setOffchainSigner(address _signer) external onlyOwner {
+        _offchainSigner = _signer;
+    }
+
+    // =========================================================================
+    // Private/Helper Functions
+    // =========================================================================
+
+    /// @dev Verify signature by recreating message hash that we signed on client side
+    /// then using that to recover the address that signed the signature
+    /// @param department department id of nft to be minted
+    /// @param _signature signature of mint request
+    /// @return true if signature is valid, false otherwise
+    function _verifySignature(
+        uint256 department,
+        bytes memory _signature
+    ) private view returns (bool) {
+        bytes32 messageHash = keccak256(
+            abi.encodePacked(msg.sender, department)
+        );
+
+        address signer = messageHash.toEthSignedMessageHash().recover(
+            _signature
+        );
+
+        return signer == _offchainSigner;
     }
 }
--- test/NUSFintech.t.sol
+++ test/NUSFintech.t.sol
@@ -3,12 +3,6 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "../src/NUSFintech.sol";
-
 contract NUSFintechTest is Test {
-    NUSFintech t;
-
-    function setUp() public {
-        t = new NUSFintech();
-    }
+    function setUp() public {}
 }
