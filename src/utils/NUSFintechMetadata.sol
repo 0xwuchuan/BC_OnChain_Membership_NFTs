56337779d30e66902670d65b8437692f68966b41
wuchuan <wuchuan@skiff.com>
2023-12-13 15:43:42 +0800

    chore: complier configs

--- src/utils/NUSFintechMetadata.sol
+++ src/utils/NUSFintechMetadata.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
 import {LibString} from "solady/src/utils/LibString.sol";
 
