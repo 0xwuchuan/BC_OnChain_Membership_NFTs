0fb4d806d4cbdb26bd0669ea67f80dc7268abfe7
Khoo Wu Chuan <77726879+0xwuchuan@users.noreply.github.com>
2023-12-16 18:36:51 +0800

    NUS Fintech Contract (#21)
    * forge install: ERC5192
    
    v0.0.1
    
    * feat: soulbound
    
    * chore: complier configs
    
    * forge install: openzeppelin-contracts
    
    v5.0.1
    
    * feat: mint logic
    
    * feat: tokenURI
    
    * feat: add .env
    
    * feat: basic test setup

--- src/utils/NUSFintechRenderer.sol
+++ src/utils/NUSFintechRenderer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import {svg} from "hot-chain-svg/SVG.sol";
 import {LibString} from 'solady/src/utils/LibString.sol';
