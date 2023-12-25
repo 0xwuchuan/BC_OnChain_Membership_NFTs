b98695f58d7ce58dd2a8b64ecbfcea90061add16
wuchuan <wuchuan@skiff.com>
2023-12-25 17:32:49 +0800

    chore: deploy config

--- script/DeployNUSFintech.s.sol
+++ script/DeployNUSFintech.s.sol
@@ -5,12 +5,14 @@ import {Script, console2} from "forge-std/Script.sol";
 import {NUSFintech} from "../src/NUSFintech.sol";
 
 contract DeployNUSFintech is Script {
+    address public immutable offchainSigner = 0xDF67f2dC7A1cbed09c0227C771fAa339E9f1A347;
+
     function run() external {
         vm.startBroadcast();
 
         NUSFintech nusFintech = new NUSFintech();
 
-        nusFintech.setOffchainSigner(0xDF67f2dC7A1cbed09c0227C771fAa339E9f1A347);
+        nusFintech.setOffchainSigner(offchainSigner);
 
         vm.stopBroadcast();
     }
